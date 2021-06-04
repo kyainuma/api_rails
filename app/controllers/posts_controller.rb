0bd1d69da69dd3cb86564a0df1fbe6b9b332f66c
Yasunari_Kainuna <yasu6491@icloud.com>
2021-06-04 09:18:57 +0900

    fix:destroyアクション時に@postをセット

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :update]
+  before_action :set_post, only: [:show, :update, :destroy]
   before_action :authenticate_user!
 
   def index
